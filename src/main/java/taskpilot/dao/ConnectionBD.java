1b8affe7105e6a1fa3bfa287b9abadd185c6c76a
luiz-henrique112 <108096881+luiz-henrique112@users.noreply.github.com>
2026-03-18 09:54:15 -0300

    Use environment variables for database connection

--- src/main/java/taskpilot/dao/ConnectionBD.java
+++ src/main/java/taskpilot/dao/ConnectionBD.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionBD {
-   private static final String URL = "";
-   private static final String USER = "";
-   private static final String PASSWORD = "";
+   private static final String URL = System.getenv("DB_URL");
+   private static final String USER = System.getenv("DB_USER");
+   private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
    public static Connection connect() {
       try {
